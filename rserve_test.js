fde5551e9086b067d28a781b5b2d4fd3743639bc
dsherry <sharshofski@gmail.com>
2013-10-06 08:18:37 -0400

    Added simple rserve test

---
+++ rserve_test.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var Rserve = require('rserve');
+
+var s = Rserve.create({
+	    //host: 'http://54.200.76.215:6311',
+ 	    host: 'http://127.0.0.1:6311/',
+        //host: 'http://ec2-54-200-76-215.us-west-2.compute.amazonaws.com:6311',
+        on_connect: test,
+        on_close: function(msg) {
+            console.log("Socket closed. (!?)");
+            console.log(msg);
+        }
+    });
+
+// s.on('error', function(e) {
+// 	console.log('ERROR:' + e);
+// };
+
+function test() {
+	console.log('CONNECTED DAMMIT');
+}
