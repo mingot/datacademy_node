baf517ac85974d130427bd1df7e95bc12378d0e5
dsherry <sharshofski@gmail.com>
2013-10-19 10:16:51 -0400

    Updated front_end.html to work with my local config, and presumably with others. Need to standardize. Also updated port in rserver_test1.js to use 8081 for compatability with ../others/rserve-js/tests/r_files/start_no_ocap

--- test_servers/rserver_test1.js
+++ test_servers/rserver_test1.js
@@ -3,7 +3,7 @@ var Rserve = require('rserve');
 
 var s = Rserve.create({
 	    //host: 'http://54.200.76.215:6311',
- 	    host: 'http://127.0.0.1:6311/',
+ 	    host: 'http://127.0.0.1:8081/',
         //host: 'http://ec2-54-200-76-215.us-west-2.compute.amazonaws.com:6311',
         on_connect: test,
         on_close: function(msg) {
