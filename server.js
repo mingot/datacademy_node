d82587db743efe8bde9eeb646a3015f3a7b80ac3
dsherry <sharshofski@gmail.com>
2013-10-19 22:48:09 -0400

    Finished "Use rserve-js instead of rio"

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var http  = require('http'),
     fs    = require('fs'),
     io    = require('socket.io'),
-    rio   = require('./rio'),
-    // rserve = require('rserve-client'),
+    rserve = require('rserve'),
     spawn = require('child_process').spawn;
 
 
 respcont = fs.readFileSync('test_servers/front_end.html');
 
+// start r connection
+console.log("Starting rserve connection");
+var r = rserve.create();
+
+// start listening for connections from django frontend
 var port = (process.env.PORT || 5000); // proces.env.PORT is the port set by Heroku to listen on
 server = http.createServer( function(req, res){
     fs.readFile('test_servers/front_end.html', function(err, page) {
@@ -18,41 +22,38 @@ server = http.createServer( function(req, res){
     
 }).listen(port);
 
-
 console.log('Server up and listening at port ' + port);
-
 
 var socket = io.listen(server);
 
-
-
-
 socket.on('connection', function(client){
 
+    // some hard-coded tests
+    // r.eval('3*4',function(a) {console.log(a);});
+    // r.eval('try(3*4foobar, silent=TRUE)',function(a) {console.log(a);});
+    // r.eval('try(fizzbizz, silent=TRUE)',function(a) {console.log(a);});
+    // r.eval('3*5',function(a) {console.log(a);});
 
     client.on('message', function(msg) {
       console.log('client has sent:' + msg);
-      //rio.evaluate(msg,Roptions);
-      rio.sourceAndEvalString(msg,Roptions);
-
+        try {
+            // if message doesn't contain an "=", this works
+            // but if it does, write eval(varname=try(rest_of_expr,silent=TRUE));
+            msg = 'try(' + msg + ', silent=TRUE)';
+            r.eval(msg, processResponse);
+        } catch (err) {
+            console.log(err);
+        }
     });
-    
+
     client.on('disconnect', function() {
       console.log('Client has disconnected');
     });
 
-    //R MANAGEMENT
-    Roptions = {
-      callback:processResponse,
-      host: "ec2-54-200-76-215.us-west-2.compute.amazonaws.com",
-      //host: "localhost",
-      port: 6311
-    };
-
-    function processResponse(err,res){
+    function processResponse(res,err){
       if(!err){
-        console.log('Response:' + res);
-        client.emit('response', res);
+        console.log('Response:' + res.value.value['0']);
+        client.emit('response', res.value.value['0']);
       }else{
         console.log('error ocurred...');
         client.emit('response', 'An error ocurred: ' + res);
