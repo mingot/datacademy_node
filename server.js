d21a14145e026d13a69a19f7990c3c41835da401
dsherry <sharshofski@gmail.com>
2013-10-21 00:17:46 -0400

    Added some plot handling code. Does not yet handle plots

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var http  = require('http'),
     rserve = require('rserve'),
     spawn = require('child_process').spawn;
 
-
 respcont = fs.readFileSync('test_servers/front_end.html');
 
 // start r connection
@@ -19,7 +18,7 @@ server = http.createServer( function(req, res){
         res.write(page);
         res.end();
     });
-    
+
 }).listen(port);
 
 console.log('Server up and listening at port ' + port);
@@ -29,21 +28,42 @@ var socket = io.listen(server);
 // a pre-parser for user input
 function expression_handler(expr) {
     // check for single-quotes, throw error if there are
-    // any, since we use them in the wrap_in_capture command
+    // any, since we use them in the wrap_capture command
     if (expr.indexOf("'") >= 0) {
         throw "Error: expression cannot contain single-quotes at this time.\nPlease use double-quotes instead.";
     }
     if (expr.substring(0,4) === 'help') {
-        return "out<-capture.output(tools:::Rd2txt(utils:::.getHelpFile(as.character(" + expr + ")))); outp = paste(out, collapse='\n'); outp";
+        return wrap_help_cmd(expr);
+    } else if (expr.substring(0,4) === 'plot') {
+        return wrap_plot_cmd(expr);
     } else {
-        return wrap_in_capture(expr);
+        return wrap_capture(expr);
     }
 }
 
+// wrap a help command with the necessary peripherals to capture output
+function wrap_help_cmd(expr) {
+    return "out<-capture.output(tools:::Rd2txt(utils:::.getHelpFile(as.character(" + expr + ")))); outp = paste(out, collapse='\n'); outp";
+}
+
 // wrap a string in with "out=capture.output(eval(try(parse(text='MY_STRING'), silent=TRUE))); out"
-function wrap_in_capture(expr) {
+function wrap_capture(expr) {
     return "out=capture.output(eval(try(parse(text='" + expr + "'), silent=TRUE))); outp = paste(out, collapse='\n'); outp";
 }
+
+// wrap a plot command to capture output
+function wrap_plot_cmd(expr) {
+    // need to wrap the expr in error handling.
+    var cmd = 'filename <- tempfile("plot", fileext = ".svg");\n' +
+        'svg(filename);\n' +
+        wrap_capture(expr) + '\n' +
+        'if (length(outp) > 1) print("Error in plot command:\n" + outp);\n' +
+        'capture.output(dev.off());\n' + // keep output from reaching stdout here
+        'image <- readBin(filename, "raw", 9999);\n' +
+        'unlink(filename);\n' +
+        'image\n'
+    return cmd;
+x}
 
 socket.on('connection', function(client){
 
@@ -74,14 +94,12 @@ socket.on('connection', function(client){
     function processResponse(res,err){
       if (!err){
           var response = res.value.value['0'];
-          var response_processed = response.replace(/\uFFFD/g, '');
           console.log('Response:' + response);
-          //response_processed = response.replace("\n", '<br>');
           // handle replacing the "_\b" with nothing in help output
-          if (response_processed.indexOf('\b') >= 0) {
-              response_processed = response_processed.replace('\b','');
+          if (response.indexOf('\b') >= 0) {
+              response = response.replace('\b','');
           }
-          client.emit('response', response_processed);
+          client.emit('response', response);
       } else {
           console.log('error ocurred...');
           client.emit('response', 'An error ocurred: ' + res);
