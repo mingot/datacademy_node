34adb52a5008cd51876c2d3449a11b29c26d7e54
Dylan Sherry <sharshofski@gmail.com>
2013-11-17 02:42:45 -0500

    Added basic multi-user support\!\! Still need to add API for new users

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 /* The following line is for disabling an unnecessary jshint warning. */
 /* http://jslinterrors.com/eval-can-be-harmful/ */
 /* jshint -W061 */
+/* also the one var deletion */
+/* jshint -W051 */
 var http  = require('http'),
 fs    = require('fs'),
 io    = require('socket.io'),
@@ -9,9 +11,18 @@ spawn = require('child_process').spawn;
 
 respcont = fs.readFileSync('test_servers/front_end.html');
 
-// start r connection
-console.log("Starting rserve connection");
-var r = rserve.create();
+// create a current user map -- just in memory for now
+var usermap = {};
+// add some default users for debugging
+new_user("user_cookie_1");
+new_user("user_cookie_2");
+new_user("user_cookie_3");
+console.log("Created usermap: %j", usermap);
+
+// start test connection to verify the rserver is up, then destroy the connection
+var r_test_connection = rserve.create();
+delete r_test_connection;
+console.log("Was able to create a test connection to rserve");
 
 // start listening for connections from django frontend
 var port = (process.env.PORT || 5000); // proces.env.PORT is the port set by Heroku to listen on
@@ -23,7 +34,7 @@ function routing_handler(req, res) {
     // do some parsing of the req.url to get the root domain
     var url_groups = req.url.split("/");
     if (url_groups.length < 2) {
-	unsupported_endpoint(req, res);
+        sendResponse(res, 400, 'URL endpoint ' + req.url + ' not supported');
     }
     // take all elements after first group
     url_groups = url_groups.slice(1, url_groups.length );
@@ -32,35 +43,76 @@ function routing_handler(req, res) {
     switch(first_url_group) {
     case '': // '/'
 	console.log('Posting locally');
-	fs.readFile('test_servers/front_end.html', function(err, page) {
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.write(page);
-	    res.end();
-	});
-	break;
+        fs.readFile('test_servers/front_end.html', function(err, page) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(page);
+            res.end();
+        });
+        break;
     case 'lib': // '/lib' for loading jquery and other front_end dependencies
 	if (url_groups.length > 2) {
-	    unsupported_endpoint(req, res);
+            sendResponse(res, 400, 'URL endpoint ' + req.url + ' not supported');
 	}
 	// use second val to retreive file
 	console.log('Posting local lib ' + req.url);
-	fs.readFile('test_servers/lib/' + url_groups[1], function(err, page) {
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.write(page);
-	    res.end();
-	});
-	break;
+        // TODO: include Content-Length?
+        fs.readFile('test_servers/lib/' + url_groups[1], function(err, page) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(page);
+            res.end();
+        });
+        break;
+    // TODO eventually add the ability to register new users here
+    // case 'login':
+    //     break;
     case 'r_eval': // '/r_eval'
-	handle_r_input(req, res);
+        // get user auth cookie
+        console.log('Headers: %j', req.headers);
+        if (!req.headers.hasOwnProperty("cookie")) {
+            sendResponse(res, 400, 'Error: no cookie header in HTTP POST request');
+        }
+
+        // get the user's user_obj
+        var user_obj = get_user_object(req.headers.cookie);
+
+        // check this user exists already
+        if (!user_obj) {
+            sendResponse(res, 400, 'Error: no user with cookie "' + req.headers.cookie + '"');
+        }
+
+        // pass this user's rserve-js connection to handle_r_input
+	handle_r_input(req, res, user_obj);
 	break;
     default:
-	unsupported_endpoint(req, res);
+        sendResponse(res, 400, 'URL endpoint ' + req.url + ' not supported');
     }
 }
 
-function unsupported_endpoint(req, res) {
-	res.writeHead(400, 'URL endpoint ' + req.url + ' not supported', {'Content-Type': 'text/plain'});
-	res.end();
+// add a new user to the usermap
+// TODO add username, other credentials? or is that front-end only?
+function new_user(user_cookie) {
+    var user_obj = {};
+    // create a new connection
+    user_obj.r_connection = rserve.create();
+    set_user_object(user_cookie, user_obj);
+}
+
+// get the user object from the usermap. return false if it doesn't exist
+function get_user_object(user_cookie) {
+    if (!(user_cookie in usermap)) {
+        return false;
+    }
+    return usermap[user_cookie];
+}
+
+// test if the usermap has a particular user's cookie
+function has_user(user_cookie) {
+    return (user_cookie in usermap);
+}
+
+// set a user object in the usermap
+function set_user_object(user_cookie, user_object) {
+    usermap[user_cookie] = user_object;
 }
 
 // a pre-parser for user input
@@ -122,55 +174,48 @@ function wrap_plot_cmd(expr) {
 // r.eval('try(fizzbizz, silent=TRUE)',function(a) {console.log(a);});
 // r.eval('3*5',function(a) {console.log(a);});
 
-function handle_r_input(http_request, http_response) {
+// a simple function to write to an HTTP response
+function sendResponse(http_response, statusCode, responseString) {
+    http_response.writeHead(statusCode, {
+	'Content-Length': responseString.length,
+	'Content-Type': 'text/plain'
+    });
+    http_response.write(responseString);
+    http_response.end();
+}
+
+function handle_r_input(http_request, http_response, user_obj) {
     // we define this callback here so it has access to the http_response object
     function processResponse(r_response_raw,err) {
 	if (!err){
-	    console.log("Raw value: %s", r_response_raw.toString());
+            console.log("Raw value: %s", r_response_raw.toString());
             var r_response = r_response_raw.value.value['0'];
             console.log('Raw response: %j', r_response_raw);
-	    if (r_response.substring(0,4) == 'plot') {
-		// handle replacing the "_\b" with nothing in help output
-		if (r_response.indexOf('\b') >= 0) {
-		    r_response = r_response.replace('\b','');
-		}
-	    }
-	    responseString = JSON.stringify({'r_response':r_response});
-	    http_response.writeHead(200, {
-		'Content-Length': responseString.length,
-		'Content-Type': 'text/plain'
-	    });
-	    http_response.write(responseString);
-	    http_response.end();
-	} else {
+            if (r_response.substring(0,4) == 'plot') {
+                // handle replacing the "_\b" with nothing in help output
+                if (r_response.indexOf('\b') >= 0) {
+                    r_response = r_response.replace('\b','');
+                }
+            }
+            responseString = JSON.stringify({'r_response':r_response});
+            sendResponse(http_response, 200, responseString);
+        } else {
             console.log('Error in response: ' + r_response_raw);
-	    responseString = JSON.stringify({'r_response':r_response_raw,
-					    'err':'An error occurred during expression eval: ' + err});
-	    http_response.writeHead(400, {
-		'Content-Length': responseString.length,
-		'Content-Type': 'text/plain'
-	    });
-	    http_response.write(responseString);
-	    http_response.end();
-	}
+            responseString = JSON.stringify({'r_response':r_response_raw,
+                                             'err':'An error occurred during expression eval: ' + err});
+            sendResponse(http_response, 400, responseString);
+        }
     }
 
     // first, only accept POSTs
     if (http_request.method == 'GET') {
-	processResponse('','Error: not configured to support HTTP GET requests for /r_eval');
-	return;
+        processResponse('','Error: not configured to support HTTP GET requests for /r_eval');
+        return;
     } else if (http_request.method == 'POST') {
-	console.log('Client has sent HTTP POST');
-        // get user auth cookie
-        console.log('Headers: %j', http_request.headers);
-        if (!http_request.headers.hasOwnProperty("cookie")) {
-	    processResponse('','Error: no cookie header in HTTP POST request');
-        }
-        // check this user exists already
-        
+        console.log('Client has sent HTTP POST');
     } else {
-	processResponse('','Error: not configured to support HTTP requests of type ' + http_request.method + ' for /r_eval');
-	return;
+        processResponse('','Error: not configured to support HTTP requests of type ' + http_request.method + ' for /r_eval');
+        return;
     }
 
     console.log('Body of post:' + http_request.body);
@@ -185,21 +230,21 @@ function handle_r_input(http_request, http_response) {
 
     http_request.on('end', function() {
 	try {
-	    console.log('Client has sent:' + fullBody);
-	    // extract expression from JSON body
-	    var body_obj = JSON.parse(fullBody);
-	    var r_cmd_raw = body_obj.r_cmd;
-	    console.log('r_cmd_raw is ' + r_cmd_raw);
+            console.log('Client has sent:' + fullBody);
+            // extract expression from JSON body
+            var body_obj = JSON.parse(fullBody);
+            var r_cmd_raw = body_obj.r_cmd;
+            console.log('r_cmd_raw is ' + r_cmd_raw);
             var r_cmd = expression_handler(r_cmd_raw);
             console.log('Evaluating command "' + r_cmd + '"');
-            r.eval(r_cmd, processResponse);
-	    return;
-	} catch (err) {
+            user_obj.r_connection.eval(r_cmd, processResponse);
+            return;
+        } catch (err) {
             console.log('Received error:');
             console.log(err);
-	    // use error handling code in processResponse
-	    processResponse('',err);
-	    return;
-	}
+            // use error handling code in processResponse
+            processResponse('',err);
+            return;
+        }
     });
 }
